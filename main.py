c35283ae8d0d5e5e324bea909d14a77e1f28257b
milad <miladbohlouli@gmail.com>
2022-05-25 15:21:33 +0200

    Visualization added

--- main.py
+++ main.py
@@ -6,27 +6,33 @@ import torch
 import numpy as np
 from torch_data_loader import IDEAL_RNN
 from model import LSTM_IDEAL
+from utils import custom_collate, visulaize, MSE_loss
+from matplotlib import pyplot as plt
+
 
 hidden_dims = 64
 batch_size = 64
 predict_seq_len = 70
-train_seq_len = 30
 stride = 100
-seq_len = 30
+total_seq_len = 30
+train_seq_len = 20
 seq_stride = 1
 lstm_hidden = 16
 ontput_size = 1
 num_epochs = 10
 
 def train():
+    # Some assertions
+    assert train_seq_len < total_seq_len
+
     train_dataset = IDEAL_RNN(
-        seq_length=seq_len,
+        seq_length=total_seq_len,
         multi_room_training=True,
         stride=stride
     )
 
     test_dataset = IDEAL_RNN(
-        seq_length=seq_len,
+        seq_length=total_seq_len,
         multi_room_training=True,
         train=False,
         stride=stride
@@ -34,6 +40,7 @@ def train():
 
     train_loader = DataLoader(
         train_dataset,
+        collate_fn=custom_collate,
         batch_size=batch_size
     )
 
@@ -42,9 +49,7 @@ def train():
         batch_size=batch_size
     )
 
-    # print(next(iter(train_loader)).shape)
-
-    input_size = train_dataset[0].shape[-1]
+    input_size = train_dataset[0][0].shape[-1]
 
     model = LSTM_IDEAL(
         hidden_dim=lstm_hidden,
@@ -57,10 +62,12 @@ def train():
 
     for i in range(num_epochs):
         losses = []
-        for sequence in train_loader:
-            sequence = sequence.float()
-            results = model(sequence[:, :-1, :])
-            mse_error = loss(results.ravel(), sequence[:, -1, 0])
+        for (sequences, dates) in train_loader:
+            sequences = sequences.float()
+            results = model(
+                sequences[:, :train_seq_len, :],
+                prediction_time_steps = (total_seq_len - train_seq_len))
+            mse_error = MSE_loss(results, sequences[:, train_seq_len:])
 
             optimizer.zero_grad()
             mse_error.backward()
@@ -68,7 +75,14 @@ def train():
 
             losses.append(mse_error.detach().numpy())
 
-        print(f"Epoch ({i}/{num_epochs}\t\tmse_error: {np.mean(losses):2.4})")
+        # Visualizing the results for train step
+        print(f"Epoch ({i}/{num_epochs}\t\tmse_error: {np.mean(losses):2.6f})")
+
+        selected = np.random.randint(0, dates.shape[0])
+        visulaize(dates[selected],
+                  target=sequences[selected].detach().numpy(),
+                  predicted=results[selected].detach().numpy())
+        plt.show()
 
 
 if __name__ == '__main__':
