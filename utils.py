b0f41ef4ee53144f68fcfdc6e2f711c932cd92a1
milad <miladbohlouli@gmail.com>
2022-05-25 15:22:43 +0200

    loss fucntion added

---
+++ utils.py
@@ -0,0 +1,47 @@
+# Todo:
+#   1. Implement the sliding and the for loop of the system for future prediction
+#   2. Implement the visualization of the temperature data
+
+from matplotlib import pyplot as plt
+import torch
+import numpy as np
+from matplotlib.dates import DateFormatter
+
+
+def visulaize(dates, target, predicted, figure_size=[20, 20], date_format=None, y_lim = None):
+    prediction_length = len(predicted)
+    if y_lim is None:
+        # Figuring put the limits
+        max_val = np.ceil(max([target.max(), predicted.max()]))
+        min_val = np.floor(min([target.min(), predicted.min()]))
+        y_limit = [min_val, max_val]
+
+    fig, ax = plt.subplots(figsize=figure_size)
+    ax.set(
+        xlabel="date(Weekday dd.mm)",
+        ylabel="C",
+        ylim=y_limit
+    )
+    ax.set_title("Visualization of the temperature data collected from households", fontsize=20)
+    if date_format is None: date_format = "%a %d.%m"
+
+    ax.plot(
+        dates,
+        target
+    )
+    ax.plot(
+        dates[-prediction_length:],
+        predicted
+    )
+    date_form = DateFormatter(date_format)
+    ax.xaxis.set_major_formatter(date_form)
+    ax.legend(["target", "predicted"])
+
+def custom_collate(batch):
+    data = torch.as_tensor(np.array([b[0] for b in batch]))
+    dates = np.array([b[1] for b in batch])
+    return data, dates
+
+
+def MSE_loss(ground_truth, results):
+    return (ground_truth - results).pow(2).sum(axis=1).mean()
